6e101d526f0428f40374bd6b5af98c2d3676914b
Maurice Bailleu <M.Bailleu@web.de>
2021-11-15 02:27:56 +0000

    Added ignore words dictionary

--- cmake/spell.cmake
+++ cmake/spell.cmake
@@ -15,7 +15,7 @@ if(FIX)
 endif()
 
 execute_process(
-    COMMAND "${SPELL_COMMAND}" ${flag}
+    COMMAND "${SPELL_COMMAND}" ${flag} --ignore-words=${CMAKE_SOURCE_DIR}/ignore-words.txt
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
     RESULT_VARIABLE result
 )
