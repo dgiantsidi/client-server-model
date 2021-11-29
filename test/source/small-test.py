6dd2f6dad5f8445077bf04f94f94181f023274ec
Maurice Bailleu <M.Bailleu@web.de>
2021-11-29 18:10:45 +0000

    Made test more stable

--- test/source/small-test.py
+++ test/source/small-test.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import queue
 import subprocess
 import threading
 import argparse
@@ -14,31 +15,46 @@ def run_client(binary_dir: str) -> int:
   """
   Runs the client binary with the given binary_dir.
   """
-  os._exit(subprocess.call([binary_dir + "/clt", "10", "localhost", str(PORT), "20000"]))
+  return subprocess.Popen([binary_dir + "/clt", "10", "localhost", str(PORT), "20000"])
 
 def run_server(binary_dir: str) -> int:
   """
   Runs the server binary with the given binary_dir.
   """
-  return subprocess.call([binary_dir + "/svr", "4", str(PORT), "localhost", "1", "10"])
+  return subprocess.Popen([binary_dir + "/svr", "4", str(PORT), "localhost", "1", "10"])
+
+
+def complete(process, q):
+  q.put(process.wait())
 
 
 def main(argv: List[str]):
+  q = queue.Queue()
   parser = argparse.ArgumentParser(description='Runs a small test of the server and client.')
   parser.add_argument('binary_dir', type=str, help='The directory containing the server and client binaries.')
   args = parser.parse_args(argv[1:])
 
-  server_thread = threading.Thread(target=run_server, args=(args.binary_dir,))
-  client_thread = threading.Thread(target=run_client, args=(args.binary_dir,))
-
-  server_thread.start()
-  sleep(0.5)
-  client_thread.start()
-
-  client_thread.join()
-  server_thread.join()
+  server = run_server(args.binary_dir)
+  client = run_client(args.binary_dir)
+  threads = [threading.Thread(target=complete, args=(p, q)) for p in [server, client]]
+  for t in threads:
+    t.start()
+  res1 = q.get()
+  server.terminate()
+  client.terminate()
+  try:
+    res2 = q.get(timeout=1)
+  except queue.Empty:
+    print("Timeout")
+    server.kill()
+    client.kill()
+    return 1
+  if res1 == 0:
+    return 0
+  return res1
+  
 
 
 
 if __name__ == "__main__":
-  main(sys.argv)
+  sys.exit(main(sys.argv))
